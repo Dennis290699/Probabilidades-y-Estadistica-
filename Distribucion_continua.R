7e4839514844634342075002e5154b80e5264246
Dennis290699 <88905858+Dennis290699@users.noreply.github.com>
2023-03-04 20:54:11 -0500

    Update Distribucion_continua.R

--- Distribucion_continua.R
+++ Distribucion_continua.R
@@ -1,26 +1,45 @@
-# Definir la población
-poblacion <- rnorm(10000, mean = 70, sd = 5)
+# DISTRIBUCION CONTINUA
+# Supongamos que se quiere conocer el peso promedio de los estudiantes universitarios 
+# de una determinada institución. Se toma una muestra aleatoria de 100 estudiantes y 
+# se registra su peso. Suponiendo que el peso sigue una distribución normal con una 
+# media de 70 kg y una desviación estándar de 5 kg, podemos aplicar el teorema del 
+# límite central para calcular la probabilidad de que la media muestral difiera del 
+# peso promedio real de la población en una cantidad determinada.
 
-# Tomar una muestra aleatoria de tamaño 100
-muestra <- sample(poblacion, 100)
+library(ggplot2)
+library(gridExtra)
 
-# Calcular la media muestral y la desviación estándar muestral
-media_muestral <- mean(muestra)
-desviacion_estandar_muestral <- sd(muestra)
+# Definir los parámetros
+n <- 100   # Tamaño de la muestra
+mu <- 70   # Media de la población
+sigma <- 5 # Desviación estándar de la población
 
-# Calcular la desviación estándar de la distribución muestral
-desviacion_estandar_distribucion_muestral <- desviacion_estandar_muestral / sqrt(100)
+# Generar una muestra aleatoria
+set.seed(123) # Para reproducibilidad
+muestra <- rnorm(n, mean = mu, sd = sigma)
 
-# Generar 1000 muestras de tamaño 100 y calcular su media muestral
-medias_muestrales <- replicate(1000, mean(sample(poblacion, 100)))
+# Calcular la media muestral y su desviación estándar
+x_barra <- mean(muestra)
+s <- sd(muestra)
 
-# Crear una gráfica con dos paneles
-par(mfrow = c(1, 2))
+# Calcular los valores de la distribución normal estándar
+z <- seq(-4, 4, length.out = 100)
+dnorm_z <- dnorm(z)
+data <- data.frame(z = z, dnorm_z = dnorm_z)
 
-# Primer panel: distribución de la muestra
-hist(muestra, main = "Distribución de la muestra", xlab = "Peso")
+# Graficar la distribución de la muestra y la distribución normal estándar
+p1 <- ggplot(data.frame(muestra = muestra), aes(x = muestra)) +
+  geom_histogram(aes(y = ..density..), binwidth = 2, color = "black", fill = "white") +
+  geom_density(alpha = 0.2, fill = "#FF6666") +
+  ggtitle("Distribución de la muestra") +
+  xlab("Peso (kg)") +
+  ylab("Densidad")
 
-# Segundo panel: distribución normal aproximada utilizando el teorema del límite central
-hist(medias_muestrales, main = "Distribución de la media muestral", xlab = "Media muestral", prob = TRUE)
-curve(dnorm(x, mean = 70, sd = desviacion_estandar_distribucion_muestral), add = TRUE, col = "red")
+p2 <- ggplot(data, aes(x = z, y = dnorm_z)) +
+  geom_line(color = "blue") +
+  ggtitle("Distribución normal estándar") +
+  xlab("Valor z") +
+  ylab("Densidad")
+
+grid.arrange(p1, p2, ncol = 2)
 
