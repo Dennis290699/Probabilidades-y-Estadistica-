302a992f715249ce58eb651a6f8d7d22ccd0aed7
Dennis290699 <88905858+Dennis290699@users.noreply.github.com>
2023-03-08 20:01:15 -0500

    Update Distribucion_discreta.R

--- Distribucion_discreta.R
+++ Distribucion_discreta.R
@@ -1,52 +1,18 @@
-# DITRIBUCION DISCRETA
-# Estamos interesados en conocer la altura promedio de estudiantes de una escuela 
-# secundaria. Para hacerlo, tomamos una muestra aleatoria de 100 estudiantes y 
-# registramos sus alturas en centímetros. Supongamos que la distribución de las 
-# alturas de los estudiantes en la población sigue una distribución discreta con 
-# media μ = 165 cm y desviación estándar σ = 8 cm.
+n <- 100   # tamaño de la muestra
+m <- 1000  # número de repeticiones
+p <- 0.5   # probabilidad de éxito en la población
 
-library(ggplot2)
-library(gridExtra)
+proporcion <- vector("numeric", length = m)
 
-# Definir los parámetros de la población
-mu <- 165   # media de la población
-sigma <- 8  # desviación estándar de la población
+for(i in 1:m){
+  proporcion[i] <- sum(rhyper(n, m*p, m*(1-p), n)) / n
+}
 
-# Generar una muestra aleatoria de 100 estudiantes
-set.seed(123) # Para que los resultados sean reproducibles
-n <- 100
-muestra <- rnorm(n, mean = mu, sd = sigma)
 
-# Calcular la media muestral y el error estándar
-media_muestral <- mean(muestra)
-error_estandar <- sigma/sqrt(n)
 
-# Crear un histograma de la muestra aleatoria
-p1 <- ggplot(data.frame(muestra), aes(x = muestra)) +
-  geom_histogram(aes(y = ..density..), binwidth = 1,
-                 fill = "#69b3a2", color = "black", alpha = 0.8) +
-  geom_vline(xintercept = media_muestral, color = "red", size = 1.5) +
-  ggtitle("Histograma de la muestra aleatoria") +
-  xlab("Altura (cm)") +
-  ylab("Densidad") +
-  theme_minimal()
+hist(proporcion, xlab = "PROPORCIÓN DE ÉXITOS", ylab = "Repeticiones", 
+     col = "#B4E4FF", main ="TLC - Hipergeométrica")
 
-# Crear una distribución normal teórica con media y desviación estándar correspondientes a los parámetros de la población
-x <- seq(mu - 4*sigma, mu + 4*sigma, length.out = 1000)
-y <- dnorm(x, mean = mu, sd = sigma)
-datos_normal <- data.frame(x, y)
-
-# Calcular el intervalo de confianza del 95%
-intervalo_confianza <- c(media_muestral - 1.96*error_estandar, media_muestral + 1.96*error_estandar)
-
-# Crear un gráfico de la distribución normal teórica
-p2 <- ggplot(datos_normal, aes(x = x, y = y)) +
-  geom_line(color = "#69b3a2", size = 1.5) +
-  geom_vline(xintercept = mu, color = "red", size = 1.5) +
-  ggtitle("Distribución normal teórica") +
-  xlab("Altura (cm)") +
-  ylab("Densidad") +
-  theme_minimal()
-
-# Combinar ambos gráficos
-grid.arrange(p1, p2, ncol = 2)
+q <- rnorm(m, mean(proporcion), sd(proporcion))
+hist(q, xlab = "proporción de éxitos", ylab = "Repeticiones", 
+     col = "#F7C8E0", main = "Normalidad")
