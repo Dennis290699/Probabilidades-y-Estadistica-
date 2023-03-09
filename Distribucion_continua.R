aa03128ec3238c212c5b0578a1a858a24b26d71c
Dennis290699 <88905858+Dennis290699@users.noreply.github.com>
2023-03-08 20:00:30 -0500

    Update Distribucion_continua.R

--- Distribucion_continua.R
+++ Distribucion_continua.R
@@ -1,49 +1,34 @@
-# DISTRIBUCION CONTINUA
-# Supongamos que se quiere conocer el peso promedio de los estudiantes universitarios 
-# de una determinada institución. Se toma una muestra aleatoria de 100 estudiantes y 
-# se registra su peso. Suponiendo que el peso sigue una distribución normal con una 
-# media de 70 kg y una desviación estándar de 5 kg, podemos aplicar el teorema del 
-# límite central para calcular la probabilidad de que la media muestral difiera del 
-# peso promedio real de la población en una cantidad determinada.
+n = 20000
 
-library(ggplot2)
-library(gridExtra)
+poblacion <- runif(n, 50, 110)
 
-# Definir los parámetros
-n <- 100   # Tamaño de la muestra
-mu <- 70   # Media de la población
-sigma <- 5 # Desviación estándar de la población
+m1 <- c()
+m2 <- c()
+m3 <- c()
 
-# Generar una muestra aleatoria
-set.seed(123) # Para reproducibilidad
-muestra <- rnorm(n, mean = mu, sd = sigma)
+for(i in 1:n){
+  m1[i] = mean(sample(poblacion, 100, replace = TRUE))
+  m2[i] = mean(sample(poblacion, 250, replace = TRUE))
+  m3[i] = mean(sample(poblacion, 500, replace = TRUE))
+}
 
-# Calcular la media muestral y su desviación estándar
-x_barra <- mean(muestra)
-s <- sd(muestra)
+par(mfrow = c(1, 3))
 
-# Calcular los valores de la distribución normal estándar
-z <- seq(-4, 4, length.out = 100)
-dnorm_z <- dnorm(z)
-data <- data.frame(z = z, dnorm_z = dnorm_z)
+# Generamos los histogramas para las tres muestras
+hist(m1, main = "Muestra de tamaño 100", xlab = "Media muestral")
+hist(m2, main = "Muestra de tamaño 250", xlab = "Media muestral")
+hist(m3, main = "Muestra de tamaño 500", xlab = "Media muestral")
 
-# Calcular el intervalo de confianza al 95%
-z_alpha_medios <- qnorm(0.975)
-IC <- x_barra + z_alpha_medios * (s / sqrt(n))
+# media_muestral <- c(m1, m2, m3)
+# media <- mean(media_muestral)
+# cat("La media de la muestra de tamaño 100 es:", media)
+# 
+# desviacion_Standart <- sd(media_muestral)
+# cat("La SD de la muestra de tamaño 100 es:", desviacion_Standart)
 
-# Graficar la distribución de la muestra y la distribución normal estándar
-p1 <- ggplot(data.frame(muestra = muestra), aes(x = muestra)) +
-  geom_histogram(aes(y = after_stat(density)), binwidth = 2, color = "black", fill = "white") +
-  geom_density(alpha = 0.2, fill = "#FF6666") +
-  ggtitle("Distribución de la muestra") +
-  xlab("Peso (kg)") +
-  ylab("Densidad")
+par(mfrow = c(1, 2))
 
-p2 <- ggplot(data, aes(x = z, y = dnorm_z)) +
-  geom_line(color = "blue") +
-  ggtitle("Distribución normal estándar") +
-  xlab("Valor z") +
-  ylab("Densidad")
+hist(m3, col = "#E3DFFD", main = "TLC", xlab = "media")
 
-grid.arrange(p1, p2, ncol = 2)
-
+w <- rnorm(n, mean(m3), sd(m3))
+hist(w, xlab = "media", ylab = "Repeticiones", col = "#FFF4D2", main = "Distribucion normal")
