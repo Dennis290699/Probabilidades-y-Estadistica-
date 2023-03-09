519d3068aafc3fbb3fc533473e63243314061a30
Dennis290699 <88905858+Dennis290699@users.noreply.github.com>
2023-03-08 20:04:24 -0500

    Update Distribucion_continua.R

--- Distribucion_continua.R
+++ Distribucion_continua.R
@@ -2,22 +2,22 @@ n = 20000
 
 poblacion <- runif(n, 50, 110)
 
-m1 <- c()
-m2 <- c()
-m3 <- c()
+arr1 <- c()
+arr2 <- c()
+arr3 <- c()
 
 for(i in 1:n){
-  m1[i] = mean(sample(poblacion, 100, replace = TRUE))
-  m2[i] = mean(sample(poblacion, 250, replace = TRUE))
-  m3[i] = mean(sample(poblacion, 500, replace = TRUE))
+  arr1[i] = mean(sample(poblacion, 100, replace = TRUE))
+  arr2[i] = mean(sample(poblacion, 250, replace = TRUE))
+  arr3[i] = mean(sample(poblacion, 500, replace = TRUE))
 }
 
 par(mfrow = c(1, 3))
 
 # Generamos los histogramas para las tres muestras
-hist(m1, main = "Muestra de tamaño 100", xlab = "Media muestral")
-hist(m2, main = "Muestra de tamaño 250", xlab = "Media muestral")
-hist(m3, main = "Muestra de tamaño 500", xlab = "Media muestral")
+hist(arr1, main = "Muestra de tamaño 100", xlab = "Media muestral")
+hist(arr2, main = "Muestra de tamaño 250", xlab = "Media muestral")
+hist(arr3, main = "Muestra de tamaño 500", xlab = "Media muestral")
 
 # media_muestral <- c(m1, m2, m3)
 # media <- mean(media_muestral)
@@ -28,7 +28,7 @@ hist(m3, main = "Muestra de tamaño 500", xlab = "Media muestral")
 
 par(mfrow = c(1, 2))
 
-hist(m3, col = "#E3DFFD", main = "TLC", xlab = "media")
+hist(arr3, col = "#E3DFFD", main = "TLC", xlab = "MEDIA")
 
-w <- rnorm(n, mean(m3), sd(m3))
-hist(w, xlab = "media", ylab = "Repeticiones", col = "#FFF4D2", main = "Distribucion normal")
+normal_datos <- rnorm(n, mean(arr3), sd(arr3))
+hist(normal_datos, xlab = "MEDIA", ylab = "Repeticiones", col = "#FFF4D2", main = "Distribucion normal")
